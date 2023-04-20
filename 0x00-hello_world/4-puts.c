714a7e99f2ea4baa88131be208ba797018190d67
jhay-smart <omalejoy83@gmail.com>
2023-04-20 07:02:40 -0700

    3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main entry
- * Return always 0
+ * main - Entry
+ * Return: Always 0
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
