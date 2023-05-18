0de5a45b70680c87084e9baf7b8f7281f6773c04
jhay-smart <omalejoy83@gmail.com>
2023-05-18 07:47:46 -0700

    commited

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 #define ERR_MSG "Error"
 
 /**
 * is_digit - checks if a string contains a non-digit char
 * @s: string to be evaluated
-*
 * Return: 0 if a non-digit is found, 1 otherwise
 */
 int is_digit(char *s)
@@ -26,7 +24,6 @@ return (1);
 /**
 * _strlen - returns the length of a string
 * @s: string to evaluate
-*
 * Return: the length of the string
 */
 int _strlen(char *s)
@@ -53,14 +50,13 @@ exit(98);
 * main - multiplies two positive numbers
 * @argc: number of arguments
 * @argv: array of arguments
-*
 * Return: always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
+int len1, len2, len, i, carry, digit1, digit2,  a = 0;
+int *result = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
@@ -79,7 +75,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1   *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
@@ -99,5 +95,4 @@ _putchar('\n');
 free(result);
 return (0);
 }
-
 
