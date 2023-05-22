c6d65020f00c04d9b144518e7a0542887c1149f5
jhay-smart <omalejoy83@gmail.com>
2023-05-22 05:53:21 -0700

    dog initialized

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,19 +3,19 @@
 
 /**
  * init_dog - Initializes a dog structure
- * @d: A dog structure.
+ * @my_dog: A dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  *
  * Return: Nothing
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *my_dog, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (my_dog != NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		my_dog->name = name;
+		my_dog->age = age;
+		my_dog->owner = owner;
 	}
 }
