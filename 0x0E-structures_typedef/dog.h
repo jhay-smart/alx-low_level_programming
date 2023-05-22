ad008e1aa01cd85c2ea7d0c859f3636283fe62e6
jhay-smart <omalejoy83@gmail.com>
2023-05-22 05:10:01 -0700

    commited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,11 +19,11 @@ struct dog
 /**
  * dog_t - Typedef for dog structure
  */
-typedef struct dog dog_t;
+typedef struct dog joy_dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void free_dog(joy_dog *d);
 
 #endif
