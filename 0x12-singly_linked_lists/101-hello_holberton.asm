f4eedc3955ee8bdf17aa29d1071f4ac55a91763f
jhay-smart <omalejoy83@gmail.com>
2023-06-01 13:16:30 -0700

    hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,26 @@
 section .data
     format db "Hello, Holberton", 0
+    newline db 10
 
 section .text
+    global main
     extern printf
-
-global main
+    extern exit
 
 main:
+    sub rsp, 8  ; Align the stack to 16-byte boundary (if needed)
+
+    ; Print the message
     mov edi, format
     xor eax, eax
     call printf
-    mov eax, 0
-    ret
+
+    ; Print a newline
+    mov edi, newline
+    xor eax, eax
+    call printf
+
+    ; Exit the program
+    xor edi, edi
+    call exit
 
